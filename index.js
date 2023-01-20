a42ea3ca9d355db89c90c33272e9a59d72b8ac3f
hindtag <mbnvargas>
2023-01-21 07:32:54 +0800

    Middle of string

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
-var a;
-var b = 2;
+// var a;
+// var b = 2;
+// console.log(a);
 
+// var a = 7;
+// b = a;
 
-var a = 7;
-b = a;
+// console.log(a);
 
-console.log(a);
+// Initalize these three variable
+    // var a;
+    // var b;
+    // var c;
+
+// Initializing
+    // var a = 5;
+    // var b = 10;
+    // var c = "I am a string";
+
+    // and
+
+    // a = a + 1; //this is equals to 6
+    // b = b + 5; //this is equals to 15
+    
+// Using proper naming convention
+    // properCamelCase
+    // myName
+
+// Using arithmetic operators
+        // +
+        // -
+        // *
+        // /
+        // % -> remainder
+
+// Augmented arithmetic operators
+        // +=
+        // -=
+        // *=
+        // /=
+
+// Incrementing numbers
+    // myVar++
+    // myAge++
+
+// Decrementing numbers
+    // myVar--
+    // myAge--
+
+// // Declaring String Variable
+//     var firstName = "Mark";
+//     var lastName = "Biron";
+//     var myFirstName = "Tag";
+
+    // Escaping a qoutes
+    //     Example:
+        //     var myStr = "I am a \"double qouted\" string inside a \"double qoutes\"";
+        // console.log(myStr);
+        //     var singleQoute = 'I am a "double qouted" string inside a "double qoutes"';
+        //     console.log(singleQoute);
+        //     var backticks = `I am a "double qouted" string inside a "double qoutes"`;
+        //     console.log(backticks);
